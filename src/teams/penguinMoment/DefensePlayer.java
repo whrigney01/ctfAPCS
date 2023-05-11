f0952994e1209c0429cb0a58fd48ccf6fb719691
whrigney01 <whrigney01@bvsd.org>
2023-05-11 14:43:29 -0600

    ooooooooooooooooooooooooooooooooooooooo

--- src/teams/penguinMoment/DefensePlayer.java
+++ src/teams/penguinMoment/DefensePlayer.java
@@ -14,11 +14,13 @@ public class DefensePlayer extends Player {
 
 	public Location getMoveLocation() {
 		boolean hasFlagStatic = false;
+
 		ArrayList<Player> team= getOtherTeam().getPlayers();
 		for (int i = 0; i < team.size(); i++) {
 			if(team.get(i).hasFlag()){
 				hasFlagStatic = true;
 			}
+
 		}
 		if(hasFlagStatic){
 			ArrayList<Player> play = this.getOtherTeam().getPlayers();
@@ -29,8 +31,31 @@ public class DefensePlayer extends Player {
 			}
 		}else{
 			ArrayList<Location> emp = this.getGrid().getEmptyAdjacentLocations(this.getLocation());
-			return emp.get((int)(Math.random()*emp.size()));
-		}
+
+			Location nextLoc = emp.get((int)(Math.random()*emp.size()));
+			if(this.getMyTeam().getFlag().getLocation().getCol() - nextLoc.getCol() - 10 >= 0 && this.getMyTeam().getFlag().getLocation().getRow() - nextLoc.getRow() - 10 >= 0){
+				return  nextLoc;
+			}else{
+				if(this.getLocation().getAdjacentLocation(this.getLocation().getDirectionToward(this.getMyTeam().getFlag().getLocation())) == null){
+					return this.getMyTeam().getFlag().getLocation();
+				}else{
+
+					int dir = this.getLocation().getDirectionToward(this.getMyTeam().getFlag().getLocation());
+					int negPos = -1;
+					for (int i = 0; i < 7; i++) {
+						dir += 45 * i * negPos;
+						negPos *= -1;
+						Location adjLoc = this.getLocation().getAdjacentLocation(dir);
+						if (this.getGrid().isValid(adjLoc)) {
+							if (this.getGrid().get(adjLoc) == null) {
+								return adjLoc;
+							}
+						}
+					}
+				}
+
+			}
+	}
 
 		return this.getLocation();
 	}
