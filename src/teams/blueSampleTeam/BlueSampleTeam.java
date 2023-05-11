e282b76354fc874e411b658f544acb5873aa33ed
whrigney01 <whrigney01@bvsd.org>
2023-05-11 15:09:25 -0600

    kikikikikikikikiikk

--- src/teams/blueSampleTeam/BlueSampleTeam.java
+++ src/teams/blueSampleTeam/BlueSampleTeam.java
@@ -13,14 +13,15 @@ public class BlueSampleTeam extends Team {
 		super(Color.BLUE);
 
 		// add your Players: add to left side - can not be too near the Flag
-		super.addPlayer(new RandomPlayer(new Location(5 + (int)(Math.random()*3 - 1), 30)));
+		//super.addPlayer(new RandomPlayer(new Location(5 + (int)(Math.random()*3 - 1), 30)));
 		super.addPlayer(new BeelinePlayer(new Location(10 + (int)(Math.random()*3 - 1), 30)));
-		super.addPlayer(new RandomPlayer(new Location(15 + (int)(Math.random()*3 - 1), 30)));
+		//super.addPlayer(new RandomPlayer(new Location(15 + (int)(Math.random()*3 - 1), 30)));
 		super.addPlayer(new BeelinePlayer(new Location(20 + (int)(Math.random()*3 - 1), 30)));
-		super.addPlayer(new RandomPlayer(new Location(30 + (int)(Math.random()*3 - 1), 30)));
+//		super.addPlayer(new RandomPlayer(new Location(30 + (int)(Math.random()*3 - 1), 30)));
 		super.addPlayer(new BeelinePlayer(new Location(35 + (int)(Math.random()*3 - 1), 30)));
-		super.addPlayer(new RandomPlayer(new Location(40 + (int)(Math.random()*3 - 1), 30)));
-		super.addPlayer(new BeelinePlayer(new Location(45 + (int)(Math.random()*3 - 1), 30)));
+//		super.addPlayer(new RandomPlayer(new Location(40 + (int)(Math.random()*3 - 1), 30)));
+		super.addPlayer(new BeelinePlayer(new Location(45 + (int)(Math.random()*3 - 1), 33)));
+		super.addPlayer(new BeelinePlayer(new Location(45 + (int)(Math.random()*3 - 1), 30)));super.addPlayer(new BeelinePlayer(new Location(45 + (int)(Math.random()*3 - 1), 32)));super.addPlayer(new BeelinePlayer(new Location(45 + (int)(Math.random()*3 - 1), 29)));super.addPlayer(new BeelinePlayer(new Location(45 + (int)(Math.random()*3 - 1), 31)));
 	}
 
 }
--- src/teams/penguinMoment/PenguinMomentTeam.java
+++ src/teams/penguinMoment/PenguinMomentTeam.java
@@ -12,13 +12,13 @@ public class PenguinMomentTeam extends Team {
 		super(Color.RED);
 
 		// add your Players: add to left side - can not be too near the Flag
-		super.addPlayer(new DefensePlayer(new Location(23, 30)));
-		super.addPlayer(new DefensePlayer(new Location(25, 30)));
-		super.addPlayer(new DefensePlayer(new Location(27, 30)));
-		super.addPlayer(new DefensePlayer(new Location(21, 30)));
-		super.addPlayer(new OffensePlayer(new Location(10, 30)));
-		super.addPlayer(new OffensePlayer(new Location(11, 30)));
-		super.addPlayer(new OffensePlayer(new Location(45, 30)));
+//		super.addPlayer(new DefensePlayer(new Location(23, 30)));
+//		super.addPlayer(new DefensePlayer(new Location(25, 30)));
+//		super.addPlayer(new DefensePlayer(new Location(27, 30)));
+//		super.addPlayer(new DefensePlayer(new Location(21, 30)));
+//		super.addPlayer(new OffensePlayer(new Location(10, 30)));
+//		super.addPlayer(new OffensePlayer(new Location(11, 30)));
+//		super.addPlayer(new OffensePlayer(new Location(45, 30)));
 		super.addPlayer(new OffensePlayer(new Location(46, 30)));
 	}
 
