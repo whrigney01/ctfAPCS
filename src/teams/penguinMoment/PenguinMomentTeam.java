39378497fea009a9b785010f6d6d4a5f1d28767f
whrigney01 <whrigney01@bvsd.org>
2023-05-19 15:05:07 -0600

    a

--- src/teams/penguinMoment/PenguinMomentTeam.java
+++ src/teams/penguinMoment/PenguinMomentTeam.java
@@ -12,14 +12,14 @@ public class PenguinMomentTeam extends Team {
 		super(Color.RED);
 
 		// add your Players: add to left side - can not be too near the Flag
-		super.addPlayer(new DefensePlayer(new Location(23, 15), 1));
-		super.addPlayer(new DefensePlayer(new Location(27, 15), -1));
-		super.addPlayer(new FrontPlayer(new Location(23, 35), 1));
-		super.addPlayer(new FrontPlayer(new Location(27, 35), -1));
+		super.addPlayer(new DefensePlayer(new Location(21, 15), -3));
+		super.addPlayer(new DefensePlayer(new Location(23, 15), -1));
+		super.addPlayer(new DefensePlayer(new Location(25, 15), 1));
+		super.addPlayer(new DefensePlayer(new Location(27, 15), 3));
 		super.addPlayer(new LurePlayer(new Location(10, 30)));
-		super.addPlayer(new OffensePlayer(new Location(11, 30)));
-		super.addPlayer(new OffensePlayer(new Location(45, 30)));
-		super.addPlayer(new OffensePlayer(new Location(46, 30)));
+		super.addPlayer(new OffensePlayer(new Location(11, 30), -1));
+		super.addPlayer(new OffensePlayer(new Location(45, 30), 1));
+		super.addPlayer(new OffensePlayer(new Location(46, 30), 1));
 	}
 
 }
